c0ea7bd2be40db3eeff693f2c3dea14f4cf87b4b
Kramerbaf <nds.kram@gmail.com>
2019-03-01 14:33:14 +0100

    changed langs

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -2,10 +2,10 @@ module.exports = [
     {
         name: 'text_B',
         storeName: {
-            en_GB: "text_B"
+            en: "text_B"
         },
         storeDesc: {
-            en_GB: ""
+            en: ""
         }
     }
 ]
